421fca721c1a97bcbf6f80c8a1fa3fc27c7e5175
Sergey Leskov <sergeylsls@gmail.com>
2018-03-02 10:30:35 +0200

    Initial Commit

---
+++ BipBop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BipBop
+//
+//  Created by Sergey Leskov on 3/2/18.
+//  Copyright © 2018 Sergey Leskov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
